81e54c418038aafb948c0931f0efe2f8ee8c549c
Amy Haywood Dutton <me@amyhaywood.com>
2025-03-26 16:28:49 -0500

    🚧 WIP: Stubbed out end of 4

---
+++ end-of-4/src/app/Document.tsx
@@ -0,0 +1,19 @@
+import { TurnstileScript } from "@redwoodjs/sdk/turnstile";
+
+export const Document: React.FC<{ children: React.ReactNode }> = ({
+  children,
+}) => (
+  <html lang="en">
+    <head>
+      <meta charSet="utf-8" />
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <title>@redwoodjs/starter-passkey-auth</title>
+      <link rel="stylesheet" href="/src/app/styles.css" />
+      <TurnstileScript />
+      <script type="module" src="/src/client.tsx"></script>
+    </head>
+    <body>
+      <div id="root">{children}</div>
+    </body>
+  </html>
+);
---
+++ end-of-4/src/worker.tsx
@@ -0,0 +1,62 @@
+import { defineApp, ErrorResponse } from "@redwoodjs/sdk/worker";
+import { index, layout, prefix } from "@redwoodjs/sdk/router";
+import { Document } from "@/app/Document";
+import { Home } from "@/app/pages/Home";
+import { setCommonHeaders } from "@/app/headers";
+import { userRoutes } from "@/app/pages/user/routes";
+import { sessions, setupSessionStore } from "./session/store";
+import { Session } from "./session/durableObject";
+import { db, setupDb } from "./db";
+import { User } from "@prisma/client";
+export { SessionDurableObject } from "./session/durableObject";
+
+export type Context = {
+  session: Session | null;
+  user: User | null;
+};
+
+export default defineApp<Context>([
+  setCommonHeaders(),
+  async ({ env, ctx, request, headers }) => {
+    await setupDb(env);
+    setupSessionStore(env);
+
+    try {
+      ctx.session = await sessions.load(request);
+    } catch (error) {
+      if (error instanceof ErrorResponse && error.code === 401) {
+        await sessions.remove(request, headers);
+        headers.set("Location", "/user/login");
+
+        return new Response(null, {
+          status: 302,
+          headers,
+        });
+      }
+
+      throw error;
+    }
+
+    if (ctx.session?.userId) {
+      ctx.user = await db.user.findUnique({
+        where: {
+          id: ctx.session.userId,
+        },
+      });
+    }
+  },
+  layout(Document, [
+    index([
+      ({ ctx }) => {
+        if (!ctx.user) {
+          return new Response(null, {
+            status: 302,
+            headers: { Location: "/user/login" },
+          });
+        }
+      },
+      Home,
+    ]),
+    prefix("/user", userRoutes),
+  ]),
+]);
